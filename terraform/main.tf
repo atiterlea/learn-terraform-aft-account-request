aea63b3b3592de6f226fa148c733ac46298bd71d
Andrei Titerlea <andrei.titerlea@liatrio.com>
2023-09-19 19:46:06 -0700

    Adding global infrastructure account

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,3 +26,32 @@ module "sandbox" {
 
   account_customizations_name = "sandbox"
 }
+
+module "infra" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "grizzacca+infra@gmail.com"
+    AccountName               = "Global Infra"
+    ManagedOrganizationalUnit = "Development"
+    SSOUserEmail              = "grizzacca@gmail.com"
+    SSOUserFirstName          = "Andrei"
+    SSOUserLastName           = "Titerlea"
+  }
+
+  account_tags = {
+    "Environment" = "Development"
+    "Workstream" = "Infra"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "HashiCorp Learn"
+    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+  }
+
+  custom_fields = {
+    group = "development"
+  }
+
+  account_customizations_name = "infra"
+}
